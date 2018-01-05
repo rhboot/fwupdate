rhboot/fwupdate

commit 6f63cdd9eabf533fce909f40bf197c699b20d363

efi: Add guid_cmp() and remove manual comparisons.
Signed-off-by: Peter Jones <pjones@redhat.com>

efi/fwupdate.c
@@ -109,6 +109,12 @@ free(void *addr, UINTN size)
 	return rc;
 }
 
+static inline int
+guid_cmp(efi_guid_t *a, efi_guid_t *b)
+{
+	return CompareMem(a, b, sizeof (*a));
+}
+
 EFI_STATUS
 read_file(EFI_FILE_HANDLE fh, UINT8 **buf_out, UINTN *buf_size_out)
 {
@@ -390,8 +396,7 @@ find_updates(UINTN *n_updates_out, update_table ***updates_out)
 		/*
 		 * If it's not one of our state variables, keep going.
 		 */
-		if (CompareMem(&vendor_guid, &fwupdate_guid,
-			       sizeof (vendor_guid)))
+		if (guid_cmp(&vendor_guid, &fwupdate_guid))
 			continue;
 
 		/*
@@ -993,14 +998,7 @@ do_ux_csum(EFI_HANDLE loaded_image, UINT8 *buf, UINTN size)
 	return EFI_SUCCESS;
 }
 
-static int
-is_ux_capsule(EFI_GUID *guid)
-{
-	if (CompareMem(guid, &ux_capsule_guid,
-		       sizeof(ux_capsule_guid)) == 0)
-		return 1;
-	return 0;
-}
+#define is_ux_capsule(guid) (guid_cmp(guid, &ux_capsule_guid) == 0)
 
 static EFI_STATUS
 add_capsule(update_table *update, EFI_CAPSULE_HEADER **capsule_out,
@@ -1032,8 +1030,7 @@ add_capsule(update_table *update, EFI_CAPSULE_HEADER **capsule_out,
 	 * Unfortunately there's not a good way to do this, so we're just
 	 * checking if the capsule has the fw_class guid at the right place.
 	 */
-	if (CompareMem(&update->info->guid, fbuf,
-			sizeof (update->info->guid)) == 0 &&
+	if (guid_cmp(&update->info->guid, (efi_guid_t *)fbuf) &&
 	    /*
 	     * We're ignoring things that are 40 bytes here, because that's
 	     * the size of the variables used in the test code I wrote for

commit 6f7c98c0f954dbd575a1c9ba75af91e0d9e36453

Fix fwupx64.efi not actually staging capsules properly. (#80)
Bisecting master shows that the problem was introduced in 6f63cdd9.

When the guid_cmp comparison was put in place it flipped the logic
of one of the checks.

git bisect start 'efi/'
 bad: [5c2a8d03a9f4d11f3624dc201ff1d2207d19e995] efi: better debugging on our capsule tests.
git bisect bad 5c2a8d03a9f4d11f3624dc201ff1d2207d19e995
 good: [04daa639f2a8a125dea0dbe8bc150d9cd1d87550] Make SUBDIRS overrideable
git bisect good 04daa639f2a8a125dea0dbe8bc150d9cd1d87550
 good: [cd9ea4cbdc93da922ce9abc45c7de63fe252ecc8] efi: delete the boot path from the BootOrder list
git bisect good cd9ea4cbdc93da922ce9abc45c7de63fe252ecc8
 good: [515b0c0800beb48e928e367d254bd14f25da034a] Fix capsule sizes for our ux image capsules
git bisect good 515b0c0800beb48e928e367d254bd14f25da034a
 bad: [6f63cdd9eabf533fce909f40bf197c699b20d363] efi: Add guid_cmp() and remove manual comparisons.
git bisect bad 6f63cdd9eabf533fce909f40bf197c699b20d363
 good: [55ed685dec24c57129de6cf66e24388d05b5ed2a] Reorganize Make.version a bit so "bumpver" works better.
git bisect good 55ed685dec24c57129de6cf66e24388d05b5ed2a
 good: [75391bc4c3dcfc384ae9527d3fd82933280a2276] Adding capsule headers should add flags if it's /not/ a ux cap.
git bisect good 75391bc4c3dcfc384ae9527d3fd82933280a2276
 first bad commit: [6f63cdd9eabf533fce909f40bf197c699b20d363] efi: Add guid_cmp() and remove manual comparisons.

efi/fwupdate.c
@@ -1037,7 +1037,7 @@ add_capsule(update_table *update, EFI_CAPSULE_HEADER **capsule_out,
 		Print(L"updates guid: %g\n", &update->info->guid);
 		Print(L"File guid: %g\n", fbuf);
 	}
-	if ((guid_cmp(&update->info->guid, (efi_guid_t *)fbuf) ||
+	if ((guid_cmp(&update->info->guid, (efi_guid_t *)fbuf) == 0 ||
 	     is_fmp_capsule((efi_guid_t *)fbuf)) &&
 	    /*
 	     * We're ignoring things that are 40 bytes here, because that's